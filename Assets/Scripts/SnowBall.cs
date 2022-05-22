66a892580d88fd9200c8b3fabf45414d5eb654e6
HuyHo <huy.ho@s3corp.com.vn>
2022-05-22 10:40:13 +0700

    Added Scene and Player Scrips, Animations

---
+++ Assets/Scripts/SnowBall.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowBall : MonoBehaviour
+{
+
+    public float ballSpeed;
+
+    public GameObject SnowballEffect;
+
+    private Rigidbody2D theRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(ballSpeed * transform.localScale.x, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Instantiate(SnowballEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
